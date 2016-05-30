78ec8f4010b392da1d18a9a3c51718a28022a335
robguevara <robguevara@paypal.com>
2016-05-30 12:59:29 -0700

    Changing code in service calls

--- src/services/implementations/pepe_set_serv_back.js
+++ src/services/implementations/pepe_set_serv_back.js
@@ -4,12 +4,13 @@ module.exports = function serviceFactory(configuration) {
     + configuration.backend_implementation.api.host + ':'
     + configuration.backend_implementation.api.port + '/'
     + configuration.backend_implementation.api.root + '/';
-    
-  function getRequest(endpoint) {    
+
+  function getRequest(endpoint) {
     function makeRequest(resolve, reject) {
       const requestConf = {
         method: 'GET',
         mode: 'cors',
+        credentials: 'include',
         headers: new Headers({
           'Content-Type': 'application/json'
         })
@@ -17,11 +18,12 @@ module.exports = function serviceFactory(configuration) {
 
       fetch(endpoint, requestConf)
         .then(function(response) {
+          console.log('SERVER RESPONSE:', response);
           if (response.status !== 200) {
             return {
               error: 'Service Failure',
               status: response.status
-            }            
+            }
           }
           return response.json();
         })
@@ -37,13 +39,45 @@ module.exports = function serviceFactory(configuration) {
             error: 'Service Failure'
           });
         });
+
+      // uncomment to test currencyConversion API http://localhost:3333/symbols
+      // or http://localhost:3000/hackauth/user_auth_server_method
+      // fetch('http://localhost:3333/symbols', requestConf)
+      //   .then(function(response) {
+      //     console.log('TEST GET REQUEST 0');
+      //     // if (response.status !== 200) {
+      //     //   return {
+      //     //     error: 'Service Failure XXX',
+      //     //     status: response.status
+      //     //   }
+      //     // }
+      //     console.log('currencyConversion response:', response);
+      //     console.log('TEST GET REQUEST 1');
+      //     return response.json();
+      //   })
+      //   .then(function(data) {
+      //     console.log('TEST GET REQUEST 2');
+      //     console.log('currencyConversion data:', JSON.stringify(data));
+      //     alert(JSON.stringify(data));
+      //     if(data.error) {
+      //       reject(data);
+      //     } else {
+      //       resolve(data);
+      //     }
+      //   })
+      //   .catch(function(reason) {
+      //     reject({
+      //       error: 'Service Failure XXX'
+      //     });
+      //   });
+
     }
-    
+
     return new Promise(function(resolve, reject) {
       makeRequest(resolve, reject);
     });
   }
-  
+
   function postRequest(enpoint, data) {
     function makeRequest(resolve, reject) {
       const requestConf = {
@@ -61,8 +95,8 @@ module.exports = function serviceFactory(configuration) {
             return {
               error: 'Service Failure',
               status: response.status
-            }            
-          }          
+            }
+          }
           return response.json();
         })
         .then(function(data) {
@@ -78,13 +112,13 @@ module.exports = function serviceFactory(configuration) {
           });
         });
     }
-    
+
     return new Promise(function(resolve, reject) {
       makeRequest(resolve, reject);
     });
 
   }
-    
+
   function getUserAuthMethod() {
     console.log('getUserAuthMethod from dev implementation');
     const endpoint = apiEndpoint
@@ -105,15 +139,15 @@ module.exports = function serviceFactory(configuration) {
       + configuration.backend_implementation.api.activate_google_auth;
     return postRequest(endpoint);
   }
-  
+
   function postAuthenticateGoogleAuth() {
     console.log('postAuthenticateGoogleAuth from dev implementation');
     const endpoint = apiEndpoint
       + configuration.backend_implementation.api.login_google_auth;
-    return postRequest(endpoint);    
-  }  
-  
-  
+    return postRequest(endpoint);
+  }
+
+
   // This object implements the bridge interface:
   // interface: {
   //   getUserAuthMethod: function(){}
